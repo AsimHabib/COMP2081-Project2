776e6e027bda9728b2275a1b23b1106fe22c9de1
AsimHabib <asimhabib2@gmail.com>
2017-02-24 10:23:57 -0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// Custom Interaction
+
+$(document).ready(function(){
+    //$("p").css("background-color", "red");
+  //  $(".x h3:first").addClass("clr-10");
+  $(".price-period h3:nth-of-type(1)").addClass("clr-10");
+  $(".price-period h3:nth-of-type(2)").addClass("clr-11");
+  $(".price-period h3:nth-of-type(3)").addClass("clr-1");
+});
